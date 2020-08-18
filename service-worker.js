c22ed1638da7536764a89421d265a16bf8c61a68
ronarmengol <ronarmengol1977@gmail.com>
2020-08-18 18:50:59 +0300

    updated service worker js file

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,68 @@
-self.addEventListener('install', (event) => {
-  console.log('Inside the UPDATED install handler:', event);
+// self.addEventListener('install', (event) => {
+//   console.log('Inside the UPDATED install handler:', event);
+// });
+  
+//   self.addEventListener('activate', (event) => {
+//     console.log('Inside the activate handler:', event);
+//   });
+  
+//   self.addEventListener(fetch, (event) => {
+//     console.log('Inside the fetch handler:', event);
+//   });
+
+
+
+
+
+  var CACHE = 'network-or-cache';
+
+ 
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+ 
+  evt.waitUntil(precache());
 });
-  
-  self.addEventListener('activate', (event) => {
-    console.log('Inside the activate handler:', event);
+
+ 
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
+
+ 
+  evt.respondWith(fromNetwork(evt.request, 400).catch(function () {
+    return fromCache(evt.request);
+  }));
+});
+
+ 
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll([
+      './controlled.html',
+      './asset'
+    ]);
   });
-  
-  self.addEventListener(fetch, (event) => {
-    console.log('Inside the fetch handler:', event);
+}
+
+ 
+function fromNetwork(request, timeout) {
+  return new Promise(function (fulfill, reject) {
+
+ 
+    var timeoutId = setTimeout(reject, timeout);
+
+ 
+    fetch(request).then(function (response) {
+      clearTimeout(timeoutId);
+      fulfill(response);
+
+ 
+    }, reject);
   });
+}
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
