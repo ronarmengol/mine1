a03926e0b582ada769fc59a4112913c80bf7585b
ronarmengol <ronarmengol1977@gmail.com>
2020-08-18 22:40:58 +0300

    added 404 file

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,41 @@
-self.addEventListener('install', (event) => {
-  console.log('Inside the UPDATED install handler:', event);
-});
+// self.addEventListener('install', (event) => {
+//   console.log('Inside the UPDATED install handler:', event);
+// });
   
-  self.addEventListener('activate', (event) => {
-    console.log('Inside the activate handler:', event);
-  });
+//   self.addEventListener('activate', (event) => {
+//     console.log('Inside the activate handler:', event);
+//   });
   
-  self.addEventListener(fetch, (event) => {
-    console.log('Inside the fetch handler:', event);
-  });
+//   self.addEventListener(fetch, (event) => {
+//     console.log('Inside the fetch handler:', event);
+//   });
 
+let CACHE_NAME = 'sw-v1'
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll('./404.html'))
+  )
+})
+self.addEventListener('fetch', (event) => {
+  if (event.request.method === 'GET') {
+    event.respondWith(
+      caches.match(event.request)
+      .then((cached) => {
+        var networked = fetch(event.request)
+          .then((response) => {
+            let cacheCopy = response.clone()
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(event.request, cacheCopy))
+            return response;
+          })
+          .catch(() => caches.match(offlinePage));
+        return cached || networked;
+      })
+    )
+  }
+  return;
+});
 
 
 
