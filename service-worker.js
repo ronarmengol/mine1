76cf521b795430d9174c0407f67926255fa09aa2
ronarmengol <ronarmengol1977@gmail.com>
2020-08-19 18:44:28 +0300

    commented out service-wroker js and register-sw js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
-self.addEventListener('install', (event) => {
-  console.log('Inside the UPDATED install handler:', event);
-});
+// self.addEventListener('install', (event) => {
+//   console.log('Inside the UPDATED install handler:', event);
+// });
   
-  self.addEventListener('activate', (event) => {
-    console.log('Inside the activate handler:', event);
-  });
+//   self.addEventListener('activate', (event) => {
+//     console.log('Inside the activate handler:', event);
+//   });
   
-  self.addEventListener(fetch, (event) => {
-    console.log('Inside the fetch handler:', event);
-  });
+//   self.addEventListener(fetch, (event) => {
+//     console.log('Inside the fetch handler:', event);
+//   });
 
 
 
